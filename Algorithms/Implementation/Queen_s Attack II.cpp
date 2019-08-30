79d391051d8d3d95a3387beecb1c21521dce94bd
mbmdm <mr.av.anisimov@gmail.com>
2019-08-30 21:35:45 +0300

    Queen's Attack II is done

--- Algorithms/Implementation/Queen_s Attack II.cpp
+++ Algorithms/Implementation/Queen_s Attack II.cpp
@@ -1,207 +1,146 @@
 #include <iostream>
 #include <vector>
+#include <fstream>
+#include <Windows.h>
 
 using namespace std;
-
-#define BOARD(x, y)  buffer[(y-1) + BOARD_size*(x-1)]
-
-char *buffer = nullptr;
-int BOARD_size;
-
-/*
-1 - queen
-2 - obstacle
-3 - posible queen way
-4 - imposible queen way
-*/
 
 enum class h_position {left, center, right, none};
 enum class v_position {above, center, bottom, none};
 
-void init(int n) {
-
-	if (buffer != nullptr)
-		delete[] buffer;
-
-	buffer = new char[n*n];
-
-	BOARD_size = n;
-
-	auto ptr = buffer;
-
-	int temp = n*n;
-
-	while (temp) {
-		*ptr = 0;
-		ptr++;
-		temp--;
-	}
-}
-
-void print_board() {
-
-	for (size_t i = 1; i <= BOARD_size; i++)
-	{
-		for (size_t j = 1; j <= BOARD_size; j++)
-		{
-			char *ptr = &BOARD(i, j);
-			cout << (int)*ptr << " ";
-		}
-		cout << endl;
-	}
-
-	cout << endl;
-
-}
-
-void mark_possible_ways(int r_q, int c_q, int marker, h_position hp, v_position vp) {
+h_position get_hposition(int r_q, int c_q, vector<int> &obstacle) {
 	
-	int row_p, colmn_p;
-
-	//mark possible ways by horisontal left
-	if ((hp == h_position::left && vp == v_position::center) || 
-		(hp == h_position::none && vp == v_position::none)) {
-		colmn_p = c_q - 1;
-		while (colmn_p) {
-			BOARD(r_q, colmn_p) = marker;
-			colmn_p--;
-		}
-	}
-
-	//mark possible ways by horisontal right
-	if ((hp == h_position::right && vp == v_position::center) ||
-		(hp == h_position::none && vp == v_position::none)) {
-		colmn_p = c_q + 1;
-		while (colmn_p <= BOARD_size) {
-			BOARD(r_q, colmn_p) = marker;
-			colmn_p++;
-		}
-	}
-
-	//mark possible ways by vertical bottom
-	if ((hp == h_position::center && vp == v_position::bottom) ||
-		(hp == h_position::none && vp == v_position::none)) {
-		row_p = r_q - 1;
-		while (row_p) {
-			BOARD(row_p, c_q) = marker;
-			row_p--;
-		}
-	}
-
-	//mark possible ways by vertical above
-	if ((hp == h_position::center && vp == v_position::above) ||
-		(hp == h_position::none && vp == v_position::none)) {
-		row_p = r_q + 1;
-		while (row_p <= BOARD_size) {
-			BOARD(row_p, c_q) = marker;
-			row_p++;
-		}
-	}
-
-	//mark possible ways by diagonal (left/bottom)
-	if ((hp == h_position::left && vp == v_position::bottom) ||
-		(hp == h_position::none && vp == v_position::none)) {
-		row_p = r_q - 1;
-		colmn_p = c_q - 1;
-		while (row_p && colmn_p) {
-			BOARD(row_p, colmn_p) = marker;
-			row_p--;
-			colmn_p--;
-		}
-	}
-
-	//mark possible ways by diagonal (right/above)
-	if ((hp == h_position::right && vp == v_position::above) ||
-		(hp == h_position::none && vp == v_position::none)) {
-		row_p = r_q + 1;
-		colmn_p = c_q + 1;
-		while (row_p <= BOARD_size && colmn_p <= BOARD_size) {
-			BOARD(row_p, colmn_p) = marker;
-			row_p++;
-			colmn_p++;
-		}
-	}
-
-	//mark possible ways by diagonal (left/above)
-	if ((hp == h_position::left && vp == v_position::above) ||
-		(hp == h_position::none && vp == v_position::none)) {
-		row_p = r_q + 1;
-		colmn_p = c_q - 1;
-		while (row_p <= BOARD_size && colmn_p) {
-			BOARD(row_p, colmn_p) = marker;
-			row_p++;
-			colmn_p--;
-		}
-	}
-
-	//mark possible ways by diagonal (right/bottom)
-	if ((hp == h_position::right && vp == v_position::bottom) ||
-		(hp == h_position::none && vp == v_position::none)) {
-		row_p = r_q - 1;
-		colmn_p = c_q + 1;
-		while (row_p && colmn_p <= BOARD_size) {
-			BOARD(row_p, colmn_p) = marker;
-			row_p--;
-			colmn_p++;
-		}
-	}
-}
-
-h_position get_hposition(int c_q, vector<int> &obstacle) {
 	if (obstacle[1] == c_q)
 		return h_position::center;
-	else if(obstacle[1] > c_q)
+
+	else if ((r_q - obstacle[0] == c_q - obstacle[1]) ||
+		(r_q + c_q == obstacle[0] + obstacle[1])) {
+		if (obstacle[1] > c_q)
+			return h_position::right;
+		else
+			return h_position::left;
+	}
+	else if (r_q == obstacle[0] && c_q > obstacle[1])
+		return h_position::left;
+	else if (r_q == obstacle[0] && c_q < obstacle[1])
 		return h_position::right;
-	else return h_position::left;
+	else 
+		return h_position::none;
 }
 
-v_position get_vposition(int r_q, vector<int> &obstacle) {
+v_position get_vposition(int r_q, int c_q, vector<int> &obstacle) {
+	
 	if (obstacle[0] == r_q)
 		return v_position::center;
-	else if (obstacle[0] > r_q)
-		return v_position::above;
-	else return v_position::bottom;
-}
 
-void past_obstacles(int r_q, int c_q, vector<vector<int>> &obstacles) {
-	
-	for (size_t i = 0; i < obstacles.size(); i++) {
-		if (BOARD(obstacles[i][0], obstacles[i][1]) == 3) {
-			BOARD(obstacles[i][0], obstacles[i][1]) = 2;
+	else if ((r_q - obstacle[0] == c_q - obstacle[1]) ||
+		(r_q + c_q == obstacle[0] + obstacle[1])) {
 
-			h_position hp = get_hposition(c_q, obstacles[i]);
-			v_position vp = get_vposition(r_q, obstacles[i]);
-
-			mark_possible_ways(obstacles[i][0], obstacles[i][1], 2, hp, vp);
-		}
-		else BOARD(obstacles[i][0], obstacles[i][1]) = 2; //just for visualization
+		if (obstacle[0] > r_q)
+			return v_position::above;
+		else
+			return v_position::bottom;
 	}
-
+	else if (c_q == obstacle[1] && r_q < obstacle[0])
+		return v_position::above;
+	else if (c_q == obstacle[1] && r_q > obstacle[0])
+		return v_position::bottom;
+	else 
+		return v_position::none;
 }
 
-int calc_queen_ways() {
-
-	int result = 0;
-
-	for (size_t i = 0; i < pow(BOARD_size, 2); i++)
-		if (buffer[i] == 3)
-			result++;
-
-	return result;
-}
+struct pt {
+	int row;
+	int column;
+};
 
 // Complete the queensAttack function below.
 int queensAttack(int n, int k, int r_q, int c_q, vector<vector<int>> obstacles) {
 
-	init(n);
+	pt top = { n, c_q };
+	pt bottom = { 1, c_q };
+	pt left = { r_q, 1 };
+	pt right = { r_q, n };
+	pt left_top;
+	{
+		int dx = min(n - r_q, c_q - 1);
+		left_top.row = r_q + dx;
+		left_top.column = c_q - dx;
+	}
+	pt right_top;
+	{
+		int dx = n - max(r_q, c_q);
+		right_top.row = r_q + dx;
+		right_top.column = c_q + dx;
+	}
+	pt left_bottom;
+	{
+		int dx = min(r_q, c_q) - 1;
+		left_bottom.row = r_q - dx;
+		left_bottom.column = c_q - dx;
+	}
+	pt right_bottom;
+	{
+		int dx = min(n - c_q, r_q - 1);
+		right_bottom.row = r_q - dx;
+		right_bottom.column = c_q + dx;
+	}
 
-	BOARD(r_q, c_q) = 1; // queen position
-	
-	mark_possible_ways(r_q, c_q, 3, h_position::none, v_position::none); //possible queen ways
-	
-	past_obstacles(r_q, c_q, obstacles); //exclude possible queen ways by obstacles
-	
-	return calc_queen_ways();
+	for (size_t i = 0; i < obstacles.size(); i++)
+	{
+		pt obst{ obstacles[i][0] , obstacles[i][1] };
+		h_position hp = get_hposition(r_q, c_q, obstacles[i]);
+		v_position vp = get_vposition(r_q, c_q, obstacles[i]);
+
+		if (hp == h_position::center && vp == v_position::above)
+			top.row = min(obst.row - 1, top.row);
+		else if (hp == h_position::center && vp == v_position::bottom)
+			bottom.row = max(obst.row + 1, bottom.row);
+		else if (hp == h_position::left && vp == v_position::center)
+			left.column = max(obst.column + 1, left.column);
+		else if (hp == h_position::right && vp == v_position::center)
+			right.column = min(obst.column - 1, right.column);
+		else if (hp == h_position::left && vp == v_position::above) {
+			if (left_top.row > obst.row - 1) {
+				left_top.row = obst.row - 1;
+				left_top.column = obst.column + 1;
+			}
+		}
+		else if (hp == h_position::right && vp == v_position::above) {
+			if (right_top.row > obst.row - 1) {
+				right_top.row = obst.row - 1;
+				right_top.column = obst.column - 1;
+			}
+		}
+		else if (hp == h_position::left && vp == v_position::bottom) {
+			if (left_bottom.row < obst.row + 1) {
+				left_bottom.row = obst.row + 1;
+				left_bottom.column = obst.column + 1;
+			}
+		}
+		else if (hp == h_position::right && vp == v_position::bottom) {
+			if (right_bottom.row < obst.row + 1) {
+				right_bottom.row = obst.row + 1;
+				right_bottom.column = obst.column - 1;
+			}
+		}
+	}
+
+	int sum = 0;
+
+	sum += right.column - left.column; //calc by horizontal
+	sum += top.row - bottom.row; //calc by vertical
+	sum += right_top.column - left_bottom.column; //calc by 1st diagonal
+	sum += right_bottom.column - left_top.column; //calc by 2nd diagonal
+
+	return sum;
+}
+
+string ExePath() {
+	char buffer[MAX_PATH];
+	GetModuleFileName(NULL, buffer, MAX_PATH);
+	string::size_type pos = string(buffer).find_last_of("\\/");
+	return string(buffer).substr(0, pos);
 }
 
 void test1() {
@@ -247,9 +186,9 @@ void test3() {
 	int res = queensAttack(n, 0, r_q, c_q, obstacles);
 
 	if (res == 9)
-		cout << "OK\t" << "test2" << endl;
+		cout << "OK\t" << "test3" << endl;
 	else
-		cout << "FAILED\t" << "test2 with value = " << res << endl;
+		cout << "FAILED\t" << "test3 with value = " << res << endl;
 }
 
 void test4() {
@@ -263,9 +202,42 @@ void test4() {
 	int res = queensAttack(n, 0, r_q, c_q, obstacles);
 
 	if (res == 10)
-		cout << "OK\t" << "test2" << endl;
+		cout << "OK\t" << "test4" << endl;
 	else
-		cout << "FAILED\t" << "test2 with value = " << res << endl;
+		cout << "FAILED\t" << "test4 with value = " << res << endl;
+}
+
+void test5(string name, int result) {
+
+	string path = ExePath();
+
+	ifstream fs{};
+	fs.open(path + "\\" + name, ios::in);
+
+	int n, k, r_q, c_q;
+
+	fs >> n;
+	fs >> k;
+	fs >> r_q;
+	fs >> c_q; 
+
+	int x, y;
+
+	vector<vector<int>> obstacles;
+	while (!fs.eof()) {
+		fs >> x;
+		fs >> y;
+		obstacles.push_back({x,y});
+	}
+
+	int res = queensAttack(n, k, r_q, c_q, obstacles);
+
+	if (res == result)
+		cout << "OK\t" << "test5" << endl;
+	else
+		cout << "FAILED\t" << "test5 with value = " << res << endl;
+
+	fs.close();
 }
 
 int main()
@@ -274,6 +246,9 @@ int main()
 	test2();
 	test3();
 	test4();
+
+	test5("data13.txt", 307303);
+	test5("data6.txt", 40);
 
 	system("pause");
 
